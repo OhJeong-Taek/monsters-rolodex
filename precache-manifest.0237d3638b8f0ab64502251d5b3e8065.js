3775dfebbe3f0f0c5ec51d5e2dafa43eb80ffc38
JeongTaek Oh <jeongtaekoh@JeongTaeks-MacBook-Pro.local>
2019-08-27 15:14:09 -0700

    Updates

---
+++ precache-manifest.0237d3638b8f0ab64502251d5b3e8065.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ca322dcadfd3144de32addf400e281f",
+    "url": "/OhJeong-Taek/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "20c267464ee7740a0f49",
+    "url": "/OhJeong-Taek/monsters-rolodex/static/css/main.ddca8d7c.chunk.css"
+  },
+  {
+    "revision": "ebdf587097504289c06e",
+    "url": "/OhJeong-Taek/monsters-rolodex/static/js/2.3adefb50.chunk.js"
+  },
+  {
+    "revision": "20c267464ee7740a0f49",
+    "url": "/OhJeong-Taek/monsters-rolodex/static/js/main.8fc25415.chunk.js"
+  },
+  {
+    "revision": "6340a8201f8243438893",
+    "url": "/OhJeong-Taek/monsters-rolodex/static/js/runtime~main.979681ff.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/OhJeong-Taek/monsters-rolodex/static/media/logo.5d5d9eef.svg"
+  }
+]);
